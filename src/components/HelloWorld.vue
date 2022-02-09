8d8765c2ca22b2129e9356d729d05631e72a9cfc
fumihub <60814954+fumihub@users.noreply.github.com>
2022-02-10 08:46:15 +0900

    test2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,6 +13,7 @@
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
         <h1 class="display-2 font-weight-bold mb-3">test test test</h1>
+        <h1 class="display-2 font-weight-bold mb-3">test test test</h1>
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
